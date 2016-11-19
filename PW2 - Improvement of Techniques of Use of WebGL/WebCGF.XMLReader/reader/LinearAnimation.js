0863d90df037c7d1588a8caf39d58d5f09e2acff
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2016-11-19 18:22:38 +0000

    'LinearAnimation' bug fixes
    Now, it is done the translation to the first control point and the
    translation and rotation mecanisms have been improved.

--- PW2 - Improvement of Techniques of Use of WebGL/WebCGF.XMLReader/reader/LinearAnimation.js
+++ PW2 - Improvement of Techniques of Use of WebGL/WebCGF.XMLReader/reader/LinearAnimation.js
@@ -27,47 +27,33 @@ function LinearAnimation(controlpoints, span) {
 LinearAnimation.prototype.calculateLineLength = function (point1, point2) {
     var sum = 0;
     for (var i = 0, delta; i < 3; i++) {
-        delta = Math.abs(point2[i] - point1[i]);
+        delta = point2[i] - point1[i];
         sum += delta * delta;
     }
     return Math.sqrt(sum);
 }
 
-/* cos(alfa) = ((x1, z1) . (x2, z2)) / (sqrt(x1^2 + z1^2) * sqrt(x2^2 + z2^2)) -> alfa in xOz */
-LinearAnimation.prototype.calculateYAngle = function (vector1, vector2) {
-    var crossProduct = 0;
-    crossProduct += vector1[0] * vector2[0];
-    crossProduct += vector1[2] * vector2[2];
-
-    var lengthVector1 = vector1[0] * vector1[0] + vector1[2] * vector1[2];
-    var lengthVector2 = vector2[0] * vector2[0] + vector2[2] * vector2[2];
-
-    var cosAlfa = crossProduct / Math.sqrt(lengthVector1 * lengthVector2);
-
-    return Math.acos(cosAlfa);
+LinearAnimation.prototype.calculateYAngle = function (point1, point2) {
+    return Math.atan2(point2[2] - point1[2], point2[0] - point1[0]);
 }
 
 LinearAnimation.prototype.calculateGeometricTransformation = function (currTime) {
-    if (this.controlpoints.length < 2)  /* static animation */
+    if (this.controlpoints.length < 2 || this.done === true)  /* static animation or animation done */
         return;
 
     this.firstTime = this.firstTime || currTime;
     var deltaTime = (currTime - this.firstTime) / 1000;   /* in seconds */
-    var translation = [0, 0, 0];
+    var translation = [0, 0, 0], yRotationAngle;
 
     if (deltaTime <= this.span) {
         var newTotalDistanceDone = this.velocity * deltaTime;
 
-        /* Calculate what is the actual pair of control points ('i' will be the second control point)
-            and add the translations already shown. Translates to the first control point in the first
-            iteration (when deltaTime = 0). */
-        var actualLineLength, i = 1, distanceCompletedLines = 0, stop = false;
-        while (!stop) {
+        /* calculate what is the actual pair of control points ('i' will be the second control point) */
+        var actualLineLength, i = 1, distanceCompletedLines = 0;
+        for (var stop = false; !stop;) {
             actualLineLength = this.calculateLineLength(this.controlpoints[i - 1], this.controlpoints[i]);
             if (distanceCompletedLines + actualLineLength <= newTotalDistanceDone) {
                 distanceCompletedLines += actualLineLength;
-                for (var j = 0; j < 3; j++)
-                    translation[j] += (this.controlpoints[i][j] - this.controlpoints[i - 1][j]);
                 i++;
             }
             else stop = true;
@@ -78,15 +64,15 @@ LinearAnimation.prototype.calculateGeometricTransformation = function (currTime)
         var lineTime = actualLineLength * this.velocity;
 
         /* calculate the line point where the object must be placed */
-        for (var j = 0; j < 3; j++)
-            translation[j] += (this.controlpoints[i][j] - this.controlpoints[i - 1][j]) * lineDeltaTime / lineTime;
+        for (var j = 0, ratio = lineDeltaTime / lineTime; j < 3; j++)
+            translation[j] = this.controlpoints[i - 1][j] + (this.controlpoints[i][j] - this.controlpoints[i - 1][j]) * ratio;
+        yRotationAngle = -this.calculateYAngle(this.controlpoints[i - 1], this.controlpoints[i]);
     } else {
-        translation = this.controlpoints[this.controlpoints.length - 1];
+        var lastIndex = this.controlpoints.length - 1;
+        translation = this.controlpoints[lastIndex];
+        yRotationAngle = -this.calculateYAngle(this.controlpoints[lastIndex - 1], this.controlpoints[lastIndex]);
         this.done = true;
     }
-
-    /* calculate Y rotation angle */
-    var yRotationAngle = -this.calculateYAngle([1, 0, 0], translation);
 
     /* generate transformation matrix */
     this.transform = mat4.create();
