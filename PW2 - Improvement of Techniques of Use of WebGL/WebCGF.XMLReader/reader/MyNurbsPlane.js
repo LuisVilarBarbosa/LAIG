6b73934b2bb9444436c0194b025ac433afcd5f44
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2016-11-19 22:30:43 +0000

    'MyNurbsPlane' is now based on 'MyNurbsPatch'

--- PW2 - Improvement of Techniques of Use of WebGL/WebCGF.XMLReader/reader/MyNurbsPlane.js
+++ PW2 - Improvement of Techniques of Use of WebGL/WebCGF.XMLReader/reader/MyNurbsPlane.js
@@ -11,49 +11,24 @@ function MyNurbsPlane(scene, dimX, dimY, partsX, partsY) {
     this.partsX = partsX;
     this.partsY = partsY;
 
-    this.init();
+    var orderU = 1;
+    var orderV = 1;
+
+    var controlPoints = [
+                [-this.dimX / 2, -this.dimY / 2, 0, 1],
+                [-this.dimX / 2, this.dimY / 2, 0, 1],
+                [this.dimX / 2, -this.dimY / 2, 0, 1],
+                [this.dimX / 2, this.dimY / 2, 0, 1]
+    ];
+
+    this.nurbsPatch = new MyNurbsPatch(this.scene, orderU, orderV, this.partsX, this.partsY, controlPoints);
 };
 
 MyNurbsPlane.prototype = Object.create(CGFobject.prototype);
 MyNurbsPlane.prototype.constructor = MyNurbsPlane;
 
-MyNurbsPlane.prototype.getKnotsVector = function (degree) {
-    var v = [];
-    for (var i = 0; i <= degree; i++)
-        v.push(0);
-    for (var i = 0; i <= degree; i++)
-        v.push(1);
-    return v;
-}
-
-MyNurbsPlane.prototype.init = function () {
-    var orderU = 1;
-    var orderV = 1;
-    var knotsU = this.getKnotsVector(orderU);
-    var knotsV = this.getKnotsVector(orderV);
-
-    var controlPoints = [
-							[
-								[-this.dimX / 2, -this.dimY / 2, 0, 1],
-								[-this.dimX / 2, this.dimY / 2, 0, 1]
-							],
-							[
-								[this.dimX / 2, -this.dimY / 2, 0, 1],
-								[this.dimX / 2, this.dimY / 2, 0, 1]
-							]
-    ]
-
-    var nurbsSurface = new CGFnurbsSurface(orderU, orderV, knotsU, knotsV, controlPoints);
-
-    getSurfacePoint = function (u, v) {
-        return nurbsSurface.getPoint(u, v);
-    };
-
-    this.nurbsObject = new CGFnurbsObject(this.scene, getSurfacePoint, this.partsX, this.partsY);
-}
-
 MyNurbsPlane.prototype.display = function () {
-    this.nurbsObject.display();
+  this.nurbsPatch.display();
 }
 
 MyNurbsPlane.prototype.setTextureCoordinates = function (lengthS, lengthT) {}
