4c9e93ed1c2ee2583c2e0fb88c5833fab74c2921
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2016-11-20 22:58:21 +0000

    Vertex shader updated to use integer values

--- PW2 - Improvement of Techniques of Use of WebGL/WebCGF.XMLReader/reader/shaders/board-vertex.glsl
+++ PW2 - Improvement of Techniques of Use of WebGL/WebCGF.XMLReader/reader/shaders/board-vertex.glsl
@@ -12,9 +12,10 @@ varying vec2 vTextureCoord;
 void main()
 {
 	vec3 offset = vec3(0.0, 0.0, 0.0);
-	vec2 f = floor(aTextureCoord * dim);
+	ivec2 f = ivec2(floor(aTextureCoord * dim));
+	ivec2 iSel = ivec2(sel);
 
-	if(f.s >= sel.s && f.t >= sel.t && f.s <= sel.s + 1.0 && f.t <= sel.t + 1.0)
+	if(f.s >= iSel.s && f.t >= iSel.t && f.s <= iSel.s + 1 && f.t <= iSel.t + 1)
 		offset.z = 0.025;
 
 	gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition + offset, 1.0);
